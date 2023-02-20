69ede2e788e2f8ea0e638315ecc6d17b308b7628
jrg4ngst3r <jrpp198@gmail.com>
2023-02-20 00:20:51 -0300

    agregue btrap, hay que arreglar color texto

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,27 @@
+//form buttons
 const addNewTask = document.querySelector("#new-task");
 const cancelTask = document.querySelector("#cancel-task");
 
+//dialog
 const addTask = document.querySelector("#add-task");
 
+//open and close dialog (x && +)
 const newTask = document.querySelector("#open-dialog");
 const closeDialog = document.querySelector("#close-dialog");
 
+//ul with tasks
 const taskList = document.querySelector("#task-list");
+
+//form and inputs
 const taskForm = document.querySelector("#task-form");
 const nameForm = document.querySelector("#form-name");
 const typeForm = document.querySelector("#form-type");
 const descForm = document.querySelector("#form-desc");
 // const prioForm = document.querySelector("#form-prio");
 
+console.log(addNewTask);
+
+let tasks = [];
 class Task {
   constructor(title, description, type, priority) {
     this._title = title;
@@ -23,8 +32,14 @@ class Task {
 
   print() {
     return `<div class="task">
-      <span>${this._title}</span>
-      
+    <div class="task__text">
+    <span>${this._title}</span>
+    <small>${this._description}</small>    
+    </div>
+      <div class="task__btns">
+          <input class="form-check-input" type="checkbox">
+        <a class="btn btn-sm btn-danger" href="#">Borrar</a>
+      </div>
     </div>`;
   }
 }
@@ -38,5 +53,14 @@ closeDialog.addEventListener("click", () => {
 });
 
 addNewTask.addEventListener("click", () => {
-  tas;
+  // let formData = new FormData(taskForm);
+  // console.log("entreee", Object.fromEntries(formData));
+  // alert(nameForm.value);
+  let nuevaTarea = new Task(
+    nameForm.value,
+    descForm.value,
+    typeForm.value,
+    "23"
+  );
+  taskList.innerHTML += nuevaTarea.print();
 });
